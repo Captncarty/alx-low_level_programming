a67af4def4f4f52a7a1173987775deed6ef0e330
Kelvin <kelvinmac7@outlook.com>
2022-09-08 19:06:02 -0700

    fixed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - This is my function
- *
- * Write a C program to the standard error
- *
- * iReturn 1
+ * main - Entry point
+ * Return: Always 1 (Success)
  */
-
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(STDOUT_FILENO, "and that piece of art is useful
+			\" - Dora Korpar, 2015-10-19\n");
 	return (1);
-}
