1159f8f3efb68c996f2264aa0b170512fdd95906
Kelvin <kelvinmac7@outlook.com>
2022-09-08 18:54:51 -0700

    Write a C program that prints, to the stderr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - This is my function
+ *
+ * Write a C program to the standard error
+ *
+ * iReturn 1
+ */
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
