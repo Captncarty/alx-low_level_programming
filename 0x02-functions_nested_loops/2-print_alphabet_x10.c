a26fb48aa4417e8bdf89a425b20757c9f000368f
Kelvin <kelvinmac7@outlook.com>
2022-09-13 19:54:19 -0700

    multiple abc

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -12,10 +12,10 @@ void print_alphabet_x10(void)
 	char j;
 	int i;
 
-	for (i = 0; i <10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		for (j = 'a'; j <= 'z'; j++)
-			putchar(j);
-		putchar('\n');
+			_putchar(j);
+		_putchar('\n');
 	}
 }
