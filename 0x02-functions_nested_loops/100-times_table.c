ad4342d20022cf9302fda61537668047c7b2e64c
Kelvin <kelvinmac7@outlook.com>
2022-09-13 20:57:34 -0700

    advanced

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,22 +1,49 @@
 #include "main.h"
-
 /**
- * print_times_table - prints times table for numbers from 0-14
- * @n: An input integer value
- * Return: Nothing
+ * print_times_table - print multiplication table up to n
+ * @n: integer argument
  */
 void print_times_table(int n)
 {
-	int i, j;
+	int row;
+	int column;
+	int product;
 
-	if (n > 0 && n < 15)
+if (n >= 0 && n <= 15)
+{
+	for (row = 0; row <= n; row++)
 	{
-		for (i = 0; i <= n; i++)
+		for (column = 0; column <= n; column++)
 		{
-			_putchar('0');
-			for (j = 1; j <= n; j++)
-				putformat(i * j);
-			_putchar('\n');
+			product = (row * column);
+			if (column == 0)
+
+				_putchar('0' + product);
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				if (product <= 9)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + product);
+				}
+				else if (product > 9 && product < 100)
+				{
+					_putchar(' ');
+					_putchar('0' + (product / 10));
+					_putchar('0' + (product % 10));
+				}
+				else if (product >= 100)
+				{
+					_putchar('0' + (product / 100));
+					_putchar('0' + ((product / 10) % 10));
+					_putchar('0' + (product % 10));
+				}
+			}
 		}
+			_putchar('\n');
 	}
+}
 }
