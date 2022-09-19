7a733a4ebf125b795e3c45335b5396ae15156cfa
Kelvin <kelvinmac7@outlook.com>
2022-09-19 16:32:10 -0700

    took pointer and value

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * reset_to_98 -  a function that takes a pointer to an int and change the value.
+ * reset_to_98 -  a function that takes a pointer
+ * to int and change the value
  * @n: An input integer
  * @reset
  * Return: Nothing
