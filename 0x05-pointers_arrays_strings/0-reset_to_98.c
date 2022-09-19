3874f7013d13742cf1d63f37fe10b413b506d37a
Kelvin <kelvinmac7@outlook.com>
2022-09-19 16:26:34 -0700

    took pointer and value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 -  a function that takes a pointer to an int and change the value.
+ * @n: An input integer
+ * @reset
+ * Return: Nothing
+ */
+
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
