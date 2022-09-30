5193436950d3e77dc54dc0f54af54ac50de28ee8
kelvin <edithmukoro.gmail.com>
2022-09-30 14:18:46 -0700

    to print my name with argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
-  * main: Point of entry
+  * main - Point of entry
   * Description - A program that prints its name, followed by a new line
-  * @argc - Argument count
-  * @argv - Argument vector
+  * @argc: Argument count
+  * @argv: Argument vector
   * Return: Always 0 (success)
   */
 
