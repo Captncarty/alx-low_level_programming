c593df58a1c96e9a95a2d369317c5bc55940ec28
kelvin <edithmukoro.gmail.com>
2022-10-04 15:13:06 -0700

    retry

--- 0x0B-malloc_free/3-main.c
+++ 0x0B-malloc_free/3-main.c
@@ -1,50 +1,43 @@
 #include "main.h"
-
 /**
- * print_grid - prints a grid of integers
- * @grid: the address of the two dimensional grid
- * @width: width of the grid
- * @height: height of the grid
- *
- * Return: Nothing.
+ * alloc_grid - allocates a grid, make space and free space
+ * @width: takes in width of grid
+ * @height: height of grid
+ * Return: grid with freed spaces
  */
-void print_grid(int **grid, int width, int height)
-{
-    int w;
-    int h;
 
-    h = 0;
-    while (h < height)
-    {
-        w = 0;
-        while (w < width)
-        {
-            printf("%d ", grid[h][w]);
-            w++;
-        }
-        printf("\n");
-        h++;
-    }   
+int **alloc_grid(int width, int height)
+{
+/*Declaring variables*/
+int **grid;
+int i, j;
+
+if (width <= 0 || height <= 0)
+{
+return (NULL);
 }
 
-/**
- * main - check the code for ALX School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    int **grid;
+grid = malloc(sizeof(int *) * height); /*malloc*/
 
-    grid = alloc_grid(6, 4);
-    if (grid == NULL)
-    {
-        return (1);
-    }
-    print_grid(grid, 6, 4);
-    printf("\n");
-    grid[0][3] = 98;
-    grid[3][4] = 402;
-    print_grid(grid, 6, 4);
-    return (0);
+if (grid == NULL)
+{
+return (NULL);
+}
+
+for (i = 0; i < height; i++)
+{
+grid[i] = malloc(sizeof(int) * width);
+if (grid[i] == NULL)
+{
+for (i = i - 1; i >= 0; i--)
+{
+free(grid[i]);
+}
+free(grid);
+return (NULL);
+}
+}
+for (i = 0; j < width; j++)
+grid[i][j] = 0;
+return (grid);
 }
