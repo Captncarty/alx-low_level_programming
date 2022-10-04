eed97ba9d81d25692e6ac43d8b3d135b4313ff0a
kelvin <edithmukoro.gmail.com>
2022-10-04 15:28:46 -0700

    splits a string into words

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -5,6 +5,8 @@
 #include <stdlib.h>
 #include <limits.h>
 
+char *argstostr(int ac, char **av);
+char **strtow(char *str);
 void free_grid(int **grid, int height);
 int **alloc_grid(int width, int height);
 char *str_concat(char *s1, char *s2);
