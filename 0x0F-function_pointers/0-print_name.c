94ae2f38a9f174828fa97842c3036a16f0efa91f
kelvin <edithmukoro.gmail.com>
2022-10-12 16:16:58 -0700

    a function pointer to print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,12 +2,13 @@
 
 /**
   * print_name - a function to print name
-  * @name - pointer name to print
-  * @f - function pointer
+  * @name: pointer name to print
+  * @f: function pointer
   * Return: Always 0 (success)
   */
 
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	if (name && f)
+		f(name);
 }
