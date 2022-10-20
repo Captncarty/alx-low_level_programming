631ce8a84aa3248fe6e5ddce11c9f122375cb848
kelvin <Kelvin>
2022-10-20 16:11:41 -0700

    fix

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,20 +1,18 @@
 #include "lists.h"
+
 /**
- * list_len - returns the number of elements in a list.
- * @h: singly linked list.
- * Return: number of elements in the list.
+ * list_len - A function that gets the number of nodes of linked list
+ * @h: head of linked list
+ * Return: number of nodes as size_t
  */
 size_t list_len(const list_t *h)
 {
-	size_t count;
+	unsigned int nodes = 0;
 
-	count = 0;
-	while (h != NULL)
+	while (h)
 	{
+		nodes++;
 		h = h->next;
-		count++;
 	}
-
-	return (count);
+	return (nodes);
 }
-
