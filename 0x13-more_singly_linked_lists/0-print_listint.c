089d52dc6b52af5451aab4e6633e2e6cab6bc24d
kelvin <Kelvin>
2022-10-24 16:03:11 -0700

    printing all elements of a list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+  * print_listint - prints all the elements of a list
+  * @h: pointer to a list
+  * Return: The number of nodes in a list
+  */
+
+size_t print_listint(const listint_t *h)
+{
+	unsigned int count;
+
+	if (h == NULL)
+		return (0);
+	for (count = 0; h != NULL; count++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (count);
+}
+
