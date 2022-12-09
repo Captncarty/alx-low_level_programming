32ece84d1179eb2a078261f9b718547baaa49f1a
maccarthy <edithmukoro3@gmail.com>
2022-12-09 18:15:26 +0100

    fix

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,51 +1,44 @@
 #include "lists.h"
-
 /**
- * insert_dnodeint_at_index - A function that inserts a node at
- * position in a list.
- * @h: The double pointer to the head.
- * @idx: The index to insert new node at.
- * @n: The data to add to new node.
- * Return: A pointer to new element, or NULL on failure.
+ *insert_dnodeint_at_index - inserts a new node at a given position
+ *@h: point to the head
+ *@idx: index of the list where the new node should be added.
+ *@n: date of the list
+ *Return: the address of the new node, or NULL if it failed
  */
+
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node = NULL, *temp = NULL;
-	unsigned int i = 0;
+	unsigned int i;
+	dlistint_t *new, *temp = *h;
 
-	new_node = malloc(sizeof(dlistint_t));
-	if (!new_node)
+	if (h == NULL)
 		return (NULL);
-	new_node->next = NULL;
-	new_node->prev = NULL;
-	new_node->n = n;
-	if (!h || !(*h))
-		*h = new_node;
-	else
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
 	{
-		temp = *h;
-		while (idx != i++ && temp->next)
-			temp = temp->next;
-		if (temp->next)
-			new_node->prev = temp->prev;
-		else
-			new_node->prev = temp;
-		if (idx == i)
-			temp->next = new_node, new_node->prev = temp;
-		else if (idx == i - 1)
-		{
-			if (temp->prev)
-				temp->prev->next = new_node;
-			else
-				*h = new_node;
-			temp->prev = new_node;
-			new_node->next = temp;
-		}
-		else
-		{
-			free(new_node);
-			return (NULL);
-		}
+		dprintf(2, "Error: Can't malloc\n");
+		return (NULL);
 	}
-	return (new_node);
+	new->n = n;
+	/* increment list to 1 before idx */
+	for (i = 0; (i < idx - 1) && (temp->next != NULL); i++)
+		temp = temp->next;
+	/* if NULL node was reached but idx wasn't */
+	if (idx - 1 > i)
+		return (NULL);
+	/* if idx was reached and... */
+	if (temp->next == NULL)
+		return (add_dnodeint_end(h, n));
+
+	new->next = temp->next; /* points new node to the one after idx */
+	temp->next = new; /* sets one prior to new to point to new */
+	new->prev = temp; /* sets new-prev */
+	temp = new->next; /* increments list to one after idx */
+	temp->prev = new; /* sets one after new's -prev to point to new */
+
+	return (new);
 }
